6985f5a8052b9f0e1e5240e81cd925d23d126821
Vyshnav kk <vyshnavkk5420@gmail.com>
2025-05-02 13:21:54 +0530

    New change made in feature1 for conflict

--- sample.c
+++ sample.c
@@ -3,6 +3,6 @@
 int main()
 {
     printf("Making new git repository\n");
-    printf("Creating a new branch in new\n");
+    printf("Creating a new branch in new with no change\n");
     return 0;
 }
